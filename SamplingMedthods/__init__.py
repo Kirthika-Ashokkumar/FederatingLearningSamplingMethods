6e9bf7d655eca3b1dd3e9451629c198ba33c1d2c
raoot <sakinabluer@gmail.com>
2024-08-08 12:42:21 -0700

    random sample function done, not been tested

--- SamplingMedthods/__init__.py
+++ SamplingMedthods/__init__.py
@@ -29,6 +29,36 @@ def cluster_sampling_w_rpeats(df_train, num_samples, fraction, samplesX, samples
         samplesy.append(np.array(samps[i][y_header]))
     
     return samps[0:num_samples]
+
+def different_random_samples(df_train, min_size, max_size, sampleX, sampley, X_header, y_header):
+    
+    if (min_size < 0 or max_size > df_train.size):
+        raise Exception("Min size cannot be negative and max size cannot be bigger than df")
+    
+    shuffle = df_train.sample(frac = 1)
+
+    samps = []
+
+    beginIdx = 0
+    nextIdx = beginIdx + random.randint(min_size, max_size)
+    count = 0
+    
+    X = np.array(df_train[X_header])
+    y = np.array(df_train[y_header])
+
+    while (nextIdx < df_train.size):
+        sizeOfSamples.append(nextIdx)
+        samplesX.append(X[beginIdx:nextIdx])
+        samplesy.append(y[beginIdx:nextIdx])
+        count = count+1
+        beginIdx = nextIdx
+        nextIdx = beginIdx + random.randint(min_size, max_size)
+
+    if (df_train.size-nextIdx > 10):
+        samplesX.append(X[nextIdx:df_train.size])
+        samplesy.append(y[nextIdx:df_train.size])
+
+    return sampley.size
     
 
 
