45d781ecbf51c785535db9d1dce3583397bd6960
raoot <sakinabluer@gmail.com>
2024-08-16 12:17:47 -0700

    completed random samples w repeats

--- SamplingMedthods/__init__.py
+++ SamplingMedthods/__init__.py
@@ -17,7 +17,7 @@ def cluster_sampling(size, df_train, num_samples, samplesX, samplesy, X_header, 
 
     return samps[0:num_samples]
 
-def cluster_sampling_w_rpeats(df_train, num_samples, fraction, samplesX, samplesy, X_header, y_header):
+def cluster_sampling_w_repeats(df_train, num_samples, fraction, samplesX, samplesy, X_header, y_header):
     shuffle = df_train.sample(frac = 1)
 
     if fraction > 1:
@@ -60,7 +60,32 @@ def different_random_samples(df_train, min_size, max_size, samplesX, samplesy, X
 
     return samplesy.size
     
+def different_random_samples_w_repeats(df_train, min_size, max_size, samplesX, samplesy, X_header, y_header):
+    
+    if (min_size < 0 or max_size > df_train.size):
+        raise Exception("Min size cannot be negative and max size cannot be bigger than df")
+    
+    # shuffle = df_train.sample(frac = 1)
+    # samps = []
+    count = 0
+    num_rows = df_train.size #added
 
+    X = np.array(df_train[X_header])
+    y = np.array(df_train[y_header])
+
+    while (samplesX.size < df_train.size):
+        beginIdx = random.randint(0, num_rows - min_size) #modified
+        sample_size = random.randint(min_size, max_size) #added
+        endIdx = min(beginIdx + sample_size, num_rows)
+        samplesX.append(X[beginIdx:endIdx])
+        samplesy.append(y[beginIdx:endIdx])
+        count = count+1
+
+  #  if (df_train.size-nextIdx > 10):
+      #  samplesX.append(X[nextIdx:df_train.size])
+      #  samplesy.append(y[nextIdx:df_train.size])
+
+    return samplesy.size
 
 
 
